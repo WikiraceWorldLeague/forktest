28ce710d009371ef2f80582308cbdd922609d00b
baro <wiki.championship@gmail.com>
2025-04-09 19:42:59 +0900

    error shoot
    error shoot

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_team_shoppingmall/cart_page.dart';
+import 'package:flutter_team_shoppingmall/cart_page_LLM.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: CartPageLLM());
+  }
+}
+
 hi
 hello
 can you read this?
 
 how about this?
+
 2nd stage
